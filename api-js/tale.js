42fc059924db8640d122e7144951a5ba7faf624a
Olga Volosatova <volya91@gmail.com>
2022-12-21 19:40:58 +0300

    Tale 1

---
+++ api-js/tale.js
@@ -0,0 +1,14 @@
+function Kolobok(text){
+    switch(text) {
+       case 'дедушка':
+           return 'Я от дедушки ушел'
+        case 'заяц':
+            return 'Я от бабушки ушел'
+        case 'лиса':
+                return 'Меня съели!'
+    }
+    }
+    console.log(Kolobok('дедушка'))
+    console.log(Kolobok('лиса'))
+    console.log(Kolobok('заяц'))
+    
